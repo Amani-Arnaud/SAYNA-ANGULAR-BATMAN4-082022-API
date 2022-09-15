65558e0de405fee077adf46cbb52c9cb4b33fdb5
Amani-Arnaud <karnauda1er@gmail.com>
2022-09-15 13:53:32 +0000

    first commit

---
+++ db.js
@@ -0,0 +1,19 @@
+const db = require('./data/db.json')
+const clients = require('./data/clients.json')
+const comments = require('./data/comments.json')
+const livraisons = require('./data/livraisons.json')
+const notes = require('./data/notes.json')
+const products = require('./data/products.json')
+const signUpUsers = require('./data/signUpUsers.json')
+const ventes = require('./data/ventes.json')
+
+module.exports = {
+    db,
+    clients,
+    comments,
+    livraisons,
+    notes,
+    products,
+    signUpUsers,
+    ventes
+}
